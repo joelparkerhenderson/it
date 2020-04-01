cd643d71a9848bdc4b4d6ad969e8828d0181febd
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-04-01 16:25:56 -0700

    Refector vars

--- projects/terraform/regional/aws_instance_demo.tf
+++ projects/terraform/regional/aws_instance_demo.tf
@@ -4,33 +4,21 @@
 #
 ##
 
-# Define local varibles that notate what the AWS free tier can do.
-locals {
-  aws_instance__instance_type__free_tier = "t2.micro"
-  aws_instance__root_block_device__volume_size__free_tier = "30"
-}
-
 # Create an EC2 instance, using our existing key pair.
 resource "aws_instance" "demo" {
   ami = data.aws_ami.ubuntu_with_current_version.id
-  instance_type = local.aws_instance__instance_type__free_tier
+  instance_type = var.aws_instance__instance_type__free_tier
   associate_public_ip_address = true
-  key_name = "administrator@joelparkerhenderson.com=98393eb0bbd83baa8eae3404da29978e"
-
+  key_name = "administrator@joelparkerhenderson.com=64454c6722400862d74e9b91f15d7033=ssh-rsa-with-automation"
   availability_zone = "us-east-1a"
-
-  # Uncomment this if you want to set the subnet.
-  #subnet_id = aws_subnet.main_public.id
-
-  # Uncomment this if you want to set the VPC security groups.
-  #vpc_security_group_ids = [aws_security_group.demo_security_group.id]
+  vpc_security_group_ids = [aws_security_group.demo.id]
 
   tags = {
     Name = "demo"
   }
 
   root_block_device {
-    volume_size           = local.aws_instance__root_block_device__volume_size__free_tier
+    volume_size           = var.aws_instance__root_block_device__volume_size__free_tier
   }
 
   # Install some typical software, by using the typical package manager.
